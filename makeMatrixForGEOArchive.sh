2b84c21ba46c2bdbd584fec3a9b01f3d7ae1fbd9
albertwcheng <albertwcheng@gmail.com>
2012-07-10 01:54:29 -0400

    commit

---
+++ makeMatrixForGEOArchive.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ $# -lt 3 ]; then
+	echo $0 inputMatrix GPLNum outputMatrix
+	exit 1
+fi
+
+inputMatrix=$1
+GPLNum=$2
+outputMatrix=$3
+
+codeDir=`absdirname.py $0`
+
+tpfile=`tempfile`
+
+if [ ! -e $codeDir/$GPLNum.mapToFeatureNum ]; then
+	echo "$codeDir/$GPLNum.mapToFeatureNum file not exist, make one using one of the raw file and cut only the first three columns"
+	exit 1
+else
+	echo "using $codeDir/$GPLNum.mapToFeatureNum"
+	joinu.py -1 .Row,.Col -2 .Row,.Col $codeDir/$GPLNum.mapToFeatureNum $inputMatrix > $tpfile
+	cuta.py -f".ID,.Status>>1-_1" $tpfile > $outputMatrix
+fi
